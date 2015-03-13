72a6f72186563c029264165e22c8dc3a029f3946
Sharmeenjahan <jahans20@students.rowan.edu>
2015-03-13 11:41:56 -0400

    fisrt commit for add project

---
+++ MongoJavaAPI/src/MongoJavaAPI.java
@@ -0,0 +1,64 @@
+/** @author Sharmeen Jahan 
+ * 
+ * Test the CRUD actions of mongodb in java API */
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
+
+
+public class MongoJavaAPI{
+   public static void main( String args[] )
+   {
+	    //First clean the database.
+	   	TestMongoDB db=new TestMongoDB();
+	   	db.InitConnection();
+	   	db.Clean();
+	   	//take the start time to get the total time it takes to complete all threads.
+	   	long begin=System.currentTimeMillis();
+	   	
+	   	int totalThreads=10;
+	   	//creating an array of threads.
+	   	Thread[] threads= new Thread[totalThreads];
+	   	
+		for (int i = 0; i < totalThreads; i++) {
+			//creating each thread with a thread number.
+			Thread t=new Thread("Thread #" + Integer.toString(i)){
+				//inserting 1000 documents in each thread.
+				public void run(){
+					TestMongoDB mon=new TestMongoDB();
+					mon.InitConnection();
+					mon.InsertRecords(1000);
+					System.out.println(" miliseconds.");
+				}
+			};
+			t.start();
+			threads[i]=t;
+		}
+		
+		//Get the total time it needs to complete the operation of all threads.
+		for(int i=0 ; i<totalThreads;i++)
+		{
+			try
+			{
+			//wait for the thread to be completed.We need that to get the total time of the whole operation.	
+			threads[i].join();
+			}
+			catch(InterruptedException e)
+			{
+			}
+			
+		}
+		//getting the end time of the operation.
+   		long end=System.currentTimeMillis();
+   		//print the time difference of the total insertion.
+   		System.out.println();
+   		System.out.println("Total:" + Long.toString(end-begin) + " miliseconds.");
+   }
+}
---
+++ MongoJavaAPI/src/TestMongoDB.java
@@ -0,0 +1,90 @@
+/**Inserts into and reads data from mongodb database.
+ * 
+ * @ Sharmeen Jahan 
+ * 
+ */
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
+
+public class TestMongoDB{
+	
+	MongoClient mongoClient;
+	DB db;
+	DBCollection coll;
+	
+	//Initialize the connection and get the database from mongodb instance.
+   public void InitConnection(){
+      try{   
+    	  // To connect to mongodb server
+         mongoClient = new MongoClient( "localhost" , 27017 );
+         // Now use to your databases
+         db = mongoClient.getDB( "test" );
+         //System.out.println("Connect to database successfully");
+        
+         coll = db.getCollection("mycol");
+         //System.out.println("Collection mycol selected successfully");
+         
+      }
+      catch (Exception e)
+      {
+    	  System.out.println(e.getMessage());
+      }
+   }
+  
+  //Insert documents into the database
+  //@param number of documents we wish to insert.
+   
+   public void InsertRecords(int totalRecords){
+	   
+	   //store the start time in millisecond.
+        long begin=System.currentTimeMillis();
+        
+        //adding multiple documents using the loop.
+        for (int i=0; i < totalRecords; i++) {
+        	
+           coll.insert(new BasicDBObject("i", i).append("name", Integer.toString(i)));
+           
+        }
+        //store the end time in millisecond.
+        long end=System.currentTimeMillis();
+        //print the time difference.
+        System.out.print(Thread.currentThread().getName() + ":"+ Long.toString(end-begin));
+	   }
+   
+   //to print records. still in progress...
+   public void ReadRecords(){
+	   DBCursor cursor1=null;
+	   
+	   try{
+        	//using a cursor to get all the docs
+        	cursor1 = coll.find();
+                
+        	while(cursor1.hasNext()) {
+               
+                }
+        }
+        catch(Exception e)
+        {
+        	System.out.println(e.getMessage());
+        }
+        
+        finally {
+                    cursor1.close();
+        }
+}
+   
+   //remove all the data from database.
+   public void Clean(){
+	   
+    	coll.remove(new BasicDBObject());
+	   
+   }
+}
